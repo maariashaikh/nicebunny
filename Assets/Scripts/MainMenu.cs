e45fbc99b01ca48fb195587f99ec753574b37dea
Maaria Shaikh <maariashaikh@Maarias-MacBook-Air.local>
2022-11-29 19:22:51 -0500

    Main menu pages and draft of score board along with music all complete

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+
+
+{
+
+    public GameObject TYM, CYC, Instructions, Menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Menu.SetActive(true);
+    }
+
+    public void SwitchPanel(GameObject Main){
+
+        Menu.SetActive(false);
+        TYM.SetActive(false);
+        CYC.SetActive(false);
+        Instructions.SetActive(false);
+        Main.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    
+    {
+         
+    }
+
+
+
+    public void QuitGame(){
+
+        Application.Quit();
+        Debug.Log("QuitGame");
+
+    }
+}
